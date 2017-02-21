b4471ef70917cda1bff6c75e10ed1921c3426a47
Kiran Challa <kichalla@microsoft.com>
2017-02-21 14:57:30 -0800

    Added controllers

--- testapp/MvcControllerPropertyAccessors/MvcControllerPropertyAccessors/Program.cs
+++ testapp/MvcControllerPropertyAccessors/MvcControllerPropertyAccessors/Program.cs
@@ -17,7 +17,6 @@ namespace MvcControllerPropertyAccessors
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
